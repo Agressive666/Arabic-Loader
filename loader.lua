58535c20dd5f1fc873f8f5fb4cc73d981a800ac3
Agressive666 <cadnapex@gmail.com>
2026-09-15 08:25:23 -0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,19 @@
+local URL = "https://arabic-loader.cadn-gta.workers.dev/"
 
+local success, source = pcall(function()
+    return game:HttpGet(URL)
+end)
+
+if not success then
+    warn("Erro ao baixar o script:", source)
+    return
+end
+
+local func, err = loadstring(source)
+
+if not func then
+    warn("Erro ao carregar o script:", err)
+    return
+end
+
+func()
